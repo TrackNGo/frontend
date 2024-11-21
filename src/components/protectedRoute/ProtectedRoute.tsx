9793c66968761f075e7b9fd81d73b2adad3b4711
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-11-21 15:32:29 +0530

    feat: protected_route

---
+++ src/components/protectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,15 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+
+function ProtectedRoute(){
+    const {isAuthenticated,loading}=useAuth()
+    if(loading){
+        if(isAuthenticated){
+            return <Outlet/>
+        }else{
+            return <Navigate to={"/home"}/>
+        }
+    }
+}
+
+export default ProtectedRoute
