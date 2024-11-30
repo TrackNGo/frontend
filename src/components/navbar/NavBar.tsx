39b7af014041b15e337a3e72be31795f7e4b2300
rashmika1111 <139681232+rashmika1111@users.noreply.github.com>
2024-11-30 15:29:00 +0530

    Update NavBar.tsx

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -18,7 +18,7 @@ const Navbar = ({ navOpen }: NavbarProps) => {
             { label: 'Fare Estimate', link: '/fareestimate', className: 'nav-link' },
             { label: 'Bus Tracking', link: '/bustracking', className: 'nav-link' },
             { label: 'Login', link: '/login', className: 'nav-link' },
-            { label: 'LnS', link: '/lnshome', className: 'nav-link' }
+            { label: 'Lost And Found', link: '/lnshome', className: 'nav-link' }
         ],
         []
     )
