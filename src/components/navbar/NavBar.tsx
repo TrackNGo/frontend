c113b19a888f3acf122c24d6b9f2140c083600df
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2024-12-06 17:53:40 +0530

    feat: update routing and navbar links for bus tracking and lost & found

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -17,7 +17,6 @@ const Navbar = ({ navOpen }: NavbarProps) => {
             { label: 'On Bus Route', link: '/busroute', className: 'nav-link' },
             { label: 'Fare Estimate', link: '/fareestimate', className: 'nav-link' },
             { label: 'Bus Tracking', link: '/bustracking', className: 'nav-link' },
-            { label: 'Login', link: '/login', className: 'nav-link' },
             { label: 'Lost And Found', link: '/lnshome', className: 'nav-link' }
         ],
         []
