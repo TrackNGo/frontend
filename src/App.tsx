75daf7709ed44205f00a973a9159bc1af8f88054
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-04-03 02:25:54 +0530

    feat: add testing route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -52,6 +52,8 @@ function App() {
             <Route path="/bustracking/routes/search/:start/:end" element={<BusTrackingBySearch />} />
             <Route path='/bustracking/details/:busNumber' element={<BusTrackingDetails />} />
 
+            <Route path='/testing' element={<Testing />} />
+
             <Route path='/LnSHome' element={<LnSHome />} />
             <Route path='/FoundItemReport' element={<FoundItemReport />} />
             <Route path='/LostItemReport' element={<LostItemReport />} />
